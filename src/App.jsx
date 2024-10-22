ba81f7d221133e85ae7283eeacccc98918324350
ponczuTM <mroczkowskioliwer10@gmail.com>
2024-10-22 13:35:42 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const [distance, setDistance] = useState("Loading...");
   const [isBlocked, setIsBlocked] = useState(false);
-  const [videoPriority, setVideoPriority] = useState(null);
-  const videoRef = useRef(null);
+  const [canReactToChange, setCanReactToChange] = useState(true);
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:3001");
@@ -13,18 +12,10 @@ const App = () => {
     ws.onmessage = (event) => {
       const data = JSON.parse(event.data);
 
-      if (!isBlocked) {
+      if (!isBlocked && canReactToChange) {
         if (["AB", "01", "02", "03", "04"].includes(data.distance)) {
           setDistance("AB");
-          setVideoPriority("video3");
           setIsBlocked(true);
-        } else if (["05", "06", "07"].includes(data.distance)) {
-          setDistance(data.distance);
-          setVideoPriority("video2");
-          setIsBlocked(true);
-        } else if (data.distance === "XX" && videoPriority !== "video3") {
-          setDistance("XX");
-          setVideoPriority("video2");
         } else {
           setDistance(data.distance);
         }
@@ -36,21 +27,56 @@ const App = () => {
     return () => {
       ws.close();
     };
-  }, [isBlocked, videoPriority]);
+  }, [isBlocked, canReactToChange]);
 
   const handleVideoEnd = () => {
     setIsBlocked(false);
-    setVideoPriority(null);
+  };
+
+  const handleVideoStart = () => {
+    setCanReactToChange(false);
+    setTimeout(() => {
+      setCanReactToChange(true);
+    }, 4000);
   };
 
   const renderContent = () => {
-    if (
-      videoPriority === "video3" ||
-      ["02", "01", "AB", "03", "04"].includes(distance)
-    ) {
+    if (["XX"].includes(distance)) {
       return (
         <video
-          ref={videoRef}
+          src="1_cz_logo.mp4"
+          autoPlay
+          loop
+          style={{
+            width: "100%",
+            height: "auto",
+            maxHeight: "100vh",
+            objectFit: "contain",
+          }}
+          volume={0.1}
+        />
+      );
+    } else if (["05", "06", "07"].includes(distance)) {
+      return (
+        <div className="video-container">
+          <video
+            src="2_cz_podejdz.mp4"
+            autoPlay
+            onPlay={handleVideoStart}
+            style={{
+              width: "100%",
+              height: "auto",
+              maxHeight: "100vh",
+              objectFit: "contain",
+            }}
+            volume={1.0}
+          />
+          <div className="message">PODEJDŹ BLIŻEJ</div>
+        </div>
+      );
+    } else if (["02", "01", "AB", "03", "04"].includes(distance)) {
+      return (
+        <video
           src="3_cz_robot.mp4"
           autoPlay
           style={{
@@ -62,33 +88,6 @@ const App = () => {
           volume={1.0}
           onEnded={handleVideoEnd}
         />
-      );
-    } else if (
-      videoPriority === "video2" ||
-      ["05", "06", "07"].includes(distance)
-    ) {
-      return (
-        <div className="video-container">
-          <video
-            ref={videoRef}
-            src="2_cz_podejdz.mp4"
-            autoPlay
-            loop={distance === "XX"}
-            style={{
-              width: "100%",
-              height: "auto",
-              maxHeight: "100vh",
-              objectFit: "contain",
-            }}
-            volume={1.0}
-            onEnded={() => {
-              if (!["XX", "05", "06", "07"].includes(distance)) {
-                handleVideoEnd();
-              }
-            }}
-          />
-          <div className="message">PODEJDŹ BLIŻEJ</div>
-        </div>
       );
     } else {
       return (
@@ -102,7 +101,7 @@ const App = () => {
             maxHeight: "100vh",
             objectFit: "contain",
           }}
-          volume={1.0}
+          volume={0.1}
         />
       );
     }
